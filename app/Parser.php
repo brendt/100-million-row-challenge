30c09b10b9180f506fb1e6d6324dd9c662748a78
brendt <brent.roose@gmail.com>
2026-02-24 12:58:59 +0100

    wip

--- app/Parser.php
+++ app/Parser.php
@@ -6,6 +6,22 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        // TODO
+        $handle = fopen($inputPath, 'r');
+        $data = [];
+
+        while ($line = fgetcsv($handle, escape: ',')) {
+            $path = str_replace('https://stitcher.io', '', $line[0]);
+            $year = substr($line[1], offset: 0, length: 10);
+            $data[$path][$year] ??= 0;
+            $data[$path][$year] += 1;
+        }
+
+        foreach ($data as &$visits) {
+            ksort($visits);
+        }
+
+        file_put_contents($outputPath, json_encode($data, JSON_PRETTY_PRINT));
+
+        fclose($handle);
     }
 }
